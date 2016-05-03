98ed775084d63b45982f91bbb5eeaf3345a352ed
Diego Anzolin Ferreira <diego.ferreira@thomsonreuters.com>
2016-05-03 13:08:38 -0300

    Better!

--- src/tlgmBot/Program.cs
+++ src/tlgmBot/Program.cs
@@ -17,12 +17,10 @@ namespace tlgmBot
 
         static async Task Run()
         {
-            var Bot = new Api("API_KEY");
+            var Bot = new Api("API-KEY");
 
             var me = await Bot.GetMe();
-
-            Console.WriteLine("Hello my name is {0}", me.Username);
-
+            
             var offset = 0;
 
             while (true)
@@ -40,19 +38,27 @@ namespace tlgmBot
                             await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
                             await Task.Delay(2000);
 
-                            if (update.Message.Text.ToLower().Contains("oi"))
+                            if (update.Message.Text.ToLower().Equals("oi"))
                             {
-                                t = await Bot.SendTextMessage(update.Message.Chat.Id, string.Format("Olá Danado(a), eu sou o {0}!", me.FirstName));
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, string.Format("Olá, eu sou o {0}!", me.FirstName));
                                 Console.WriteLine("Echo Message: {0}", update.Message.Text);
                             }
                             else if (update.Message.Text.ToLower().Contains("bom dia"))
                             {
-                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Bom dia Danados(as)!");
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Bom dia Danado(a)!");
                             }
                             else if (update.Message.Text.ToLower().Contains("doideira"))
                             {
                                 t = await Bot.SendTextMessage(update.Message.Chat.Id, "KKK");
                                 t = await Bot.SendTextMessage(update.Message.Chat.Id, "Doideira hein?");
+                            }
+                            else if (update.Message.Text.ToLower().Contains("danado"))
+                            {
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Danadoooooooooo!");
+                            }
+                            else if (update.Message.Text.ToLower().Contains("danada"))
+                            {
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Danadaaaaaaaaaa!");
                             }
                             else
                             {
@@ -61,7 +67,9 @@ namespace tlgmBot
                             }
                             break;
 
-                        case Telegram.Bot.Types.ChatType.Group:
+                        case ChatType.Channel:
+                        case ChatType.Group:
+                        case ChatType.Supergroup:
 
                             await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
                             await Task.Delay(2000);
@@ -77,6 +85,20 @@ namespace tlgmBot
                                 t = await Bot.SendTextMessage(update.Message.Chat.Id, "Doideira hein?");
                             }
 
+                            if (update.Message.Text.ToLower().Contains("danado"))
+                            {
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Danadoooooooooo!");
+                            }
+
+                            if (update.Message.Text.ToLower().Contains("danada"))
+                            {
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Danadaaaaaaaaaa!");
+                            }
+
+                            if (update.Message.Text == "/start")
+                            {
+                                t = await Bot.SendTextMessage(update.Message.Chat.Id, "Oi!");
+                            }
                             if (update.Message.Text == "/command1")
                             {
                                 t = await Bot.SendTextMessage(update.Message.Chat.Id, "Oi Danado(a)!");
